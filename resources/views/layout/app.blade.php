3e9d66b846ee8c5e3db8fcdc170e1fca9bdd4eb3
be-tho <56642267+be-tho@users.noreply.github.com>
2024-07-07 14:04:47 -0300

    primer commit
    subida de archivos base

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="es_ES">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jr2 | @yield('title')</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Acerca de</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="container">
+        @yield('content')
+    </main>
+    <footer>
+        <p>&copy; {{ date('Y') }} Jr2</p>
+    </footer>
+</body>
+</html>
---
+++ resources/views/sections/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app')
+@section('title', 'Home')
+@section('content')
+    <h1 class="text-4xl text-center">Home</h1>
+@endsection

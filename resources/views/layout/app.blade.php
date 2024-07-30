ee1d359da90bd480bb2bab4d63f67cfc5f549d70
Albert Villarroel <56642267+be-tho@users.noreply.github.com>
2024-07-30 20:56:05 -0300

    corte: show corte with img modal

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -108,11 +108,11 @@
     </div>
     @yield('content')
 </main>
-<footer class="bg-white rounded-lg shadow dark:bg-gray-800 fixed left-0 bottom-0 right-0">
+<footer class="bg-gray-800 text-white rounded-lg shadow dark:bg-gray-800 fixed left-0 bottom-0 right-0">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline">JR2™</a>. Todos los derechos reservados.
+      <span class="text-sm text-white sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline">JR2™</a>. Todos los derechos reservados.
     </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white dark:text-gray-400 sm:mt-0">
             <li>
                 <a href="#" class="hover:underline me-4 md:me-6">Politicas de privacidad</a>
             </li>

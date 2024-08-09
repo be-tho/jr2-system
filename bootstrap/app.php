240ad4efc8013af5148cd085edb15eb2d7d7323e
Albert Villarroel <56642267+be-tho@users.noreply.github.com>
2024-08-08 22:49:16 -0300

    middleware: register auth middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Middleware\Authenticate;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthManager;
+
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,8 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'auth' => Authenticate::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
